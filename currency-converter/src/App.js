28a0081665c4f83ab218c910a4eb6f09bd1badc2
Eduardo Seije <eduardoseije@gmail.com>
2021-08-19 09:42:20 -0300

    React Structure and CurrencyRow component with an number input and a select

---
+++ currency-converter/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+function App() {
+  return (
+    <>
+      <h1> ReactJS - Currency Converter </h1>  
+      <CurrencyRow />
+      <div>=</div>
+      <CurrencyRow />
+    </>
+  );
+}
+
+export default App;
---
+++ currency-converter/src/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+  return (
+    <div>
+      <input type="number" />
+      <select>
+        <option value="HI">HI</option>
+      </select>
+    </div>
+  )
+}
