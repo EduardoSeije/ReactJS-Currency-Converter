7be599ed4d5aca789327e091ffea93942dce938e
Eduardo Seije <eduardoseije@gmail.com>
2021-08-19 10:02:41 -0300

    Fetching the exchange api using useEffect

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -1,12 +1,21 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=d91344f8fca1d3a56c7c4b27e7efc297' 
+
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, []);
+
   return (
     <>
       <h1> ReactJS - Currency Converter </h1>  
       <CurrencyRow />
-      <div>=</div>
+      <div className="equals">=</div>
       <CurrencyRow />
     </>
   );
