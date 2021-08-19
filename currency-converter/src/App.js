280a7dfbd10860f921e97e9912ba30269ed3efc5
Eduardo Seije <eduardoseije@gmail.com>
2021-08-19 10:24:10 -0300

    Create new state to store the from currency and to currency, also created a onChangeCurrency function to change the select input values

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -6,12 +6,17 @@ const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=d91344f8fc
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState([]);
+  const [toCurrency, setToCurrency] = useState([]);
 
   useEffect(() => {
     fetch(BASE_URL)
-    .then(res => res.json())
-    .then(data => {
-      setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
     })
   }, []);
 
@@ -20,10 +25,14 @@ function App() {
       <h1> ReactJS - Currency Converter </h1>  
       <CurrencyRow 
         currencyOptions={ currencyOptions }
+        selectedCurrency={ fromCurrency }
+        onChangeCurrency={ e => setToCurrency(e.target.value) }
       />
       <div className="equals">=</div>
       <CurrencyRow 
         currencyOptions={ currencyOptions }
+        selectedCurrency={ toCurrency }
+        onChangeCurrency={ e => setToCurrency(e.target.value) }
       />
     </>
   );
--- currency-converter/src/CurrencyRow.js
+++ currency-converter/src/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function CurrencyRow(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency 
+  } = props;
   return (
     <div>
       <input type="number" className="input"/>
-      <select>
-        <option value="HI">HI</option>
+      <select value={ selectedCurrency } onChange={ onChangeCurrency }>
+        {currencyOptions.map(option => (
+          <option key={option} value={option}>{option}</option>))}
       </select>
     </div>
   )
